87ffec9c851a73bb584455da9292bf05adbc12d0
tamajit <guharoytamajit@gmail.com>
2018-09-11 16:25:10 +0530

    initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String home(Model model,
+                       @RequestParam(value = "name", required = false,
+                               defaultValue = "Guest") String name) {
+
+        model.addAttribute("name", name);
+        model.addAttribute("msg", "hello world");
+        model.addAttribute("msg2", "hello world2");
+        return "home";
+
+    }
+}
